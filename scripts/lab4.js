d52b48fe0c8719d0fa3568f0b6e73ffaed730bdb
adarsh249 <adarshpatel249@gmail.com>
2024-04-22 10:38:53 -0700

    updating readme and lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -1,9 +1,9 @@
 /**
  * 
- * @param {*} num1 
- * @param {*} num2 
- * @param {*} add 
- * @returns Sum of two numbers or false if add is false
+ * @param {*} num1, first number to add. 
+ * @param {*} num2, second number to add. 
+ * @param {*} add, boolean value to tell the function what to do. 
+ * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
     if (add) {
@@ -20,9 +20,9 @@ function sumValues(num1, num2, add) {
 
 /**
  * 
- * @param {*} prices 
- * @param {*} discount 
- * @returns An array of each price's new price. If prices array is empty, return false.
+ * @param {*} prices, an array of the original price.
+ * @param {*} discount, a number between 0-1 to represent the discount. 
+ * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
     const discounted = []
